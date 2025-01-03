2feacc3cb5949886be182dd73c486bc33411f450
Javier Montes Carrera <javier.montes.carrera@gmail.com>
2025-01-02 20:38:23 -0500

    feat: Add tag component

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,3 +1,4 @@
+import Tag from "../Tag";
 import styles from "./Banner.module.css";
 
 const Banner = () => {
@@ -7,6 +8,9 @@ const Banner = () => {
       style={{ backgroundImage: 'url("/img/Banner.png")' }}
     >
         <div className={styles.gradient}>
+        </div>
+        <div style={{position: "absolute", top: "50%", transform: "translate(40.11px, -550px)"}}>
+          <Tag titulo="Front End" color="#6BD1FF" alto={92} fontsize={48} ancho={297} />
         </div>
     </div>
   );
--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,2 +1,24 @@
 import style from "./Tag.module.css";
 
+const Tag = (
+                {titulo, color, alto, fontsize, ancho}:
+                {titulo: string; color: string, alto?: number, fontsize?: number, ancho?: number}
+            ) => {
+    return (
+        <div 
+            className={style.tag} 
+            style={
+                    {
+                        background: `${color}`,
+                        height: `${(alto) ? alto : 70}px`,
+                        fontSize: `${(fontsize) ? fontsize : 32}px`,
+                        width: `${(ancho) ? ancho : 432}px`
+                    }
+                }
+        >
+            <p>{titulo}</p>
+        </div>
+    );
+};
+
+export default Tag;
