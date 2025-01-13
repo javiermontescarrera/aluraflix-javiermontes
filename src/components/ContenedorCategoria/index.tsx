217e6c2675ab420f5773bed27c2cbdb1c534e69c
Javier Montes Carrera <javier.montes.carrera@gmail.com>
2025-01-12 21:45:51 -0500

    Added color to categoria scrollbar

--- src/components/ContenedorCategoria/index.tsx
+++ src/components/ContenedorCategoria/index.tsx
@@ -2,6 +2,8 @@ import Tag from "../Tag";
 import styles from "./ContenedorCategoria.module.css"
 import { categoriaType, videoType } from "../../context/AluraFlix";
 import MiniaturaVideo from "../MiniaturaVideo";
+import hexToRgba from "hex-to-rgba";
+import { backdropClasses } from "@mui/material";
 
 
 const ContenedorCategoria = (
@@ -15,8 +17,14 @@ const ContenedorCategoria = (
         videoClick: (video: videoType) => void
     }) => {
 
+
+    const styleCategoria = {
+        backgroundColor: `transparent`,
+        '--color-categoria': hexToRgba(categoria.color, 1),
+    };
+
     return (
-        <div className={styles.categoria}>
+        <div className={styles.categoria} style={styleCategoria}>
             <Tag titulo={categoria.nombre} color={categoria.color} />
             <div className={styles.listaVideos}>
                 {
